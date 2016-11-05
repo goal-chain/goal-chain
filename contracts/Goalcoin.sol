5ccbf68b80d214d8cbb1aec8f007035de9e1a6c5
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2016-11-05 13:44:35 +0000

    GoalContract

---
+++ contracts/Goalcoin.sol
@@ -0,0 +1,12 @@
+
+
+contract Goalcoin {
+    /* Define variable owner of the type address*/
+    address owner;
+
+    /* this function is executed at initialization and sets the owner of the contract */
+    function Goalcoin() { owner = msg.sender; }
+
+    /* Function to recover the funds on the contract */
+    function kill() { if (msg.sender == owner) selfdestruct(owner); }
+}
