aa4efd92542ceb98184daefb9caff1163064e2c2
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2016-11-05 22:28:13 +0000

    First version of the escrow

--- contracts/Goal.sol
+++ contracts/Goal.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.4;
 
+import 'Escrow.sol';
+
 contract Goal {
   enum State {Active, Achieved, Failed, Closed}
   State public state;
@@ -10,6 +12,8 @@ contract Goal {
   uint public amount;
   string public accessToken;
   address public payoutAddress;
+  uint private currentSteps;
+  Escrow public escrow;
 
   function Goal(address _ownerAddress, string _accessToken, uint _initialSteps, uint _goalSteps, uint _completionDate, uint _amount, address _payoutAddress) {
     state = State.Active;
@@ -20,13 +24,21 @@ contract Goal {
     completionDate = _completionDate;
     amount = _amount;
     payoutAddress = _payoutAddress;
+    escrow = new Escrow(_ownerAddress, _payoutAddress);
   }
 
-  function updateSteps(uint currentSteps) {
+  function checkProgress(){
+    //uint8 progress = 
+  }
+
+
+  function updateSteps(uint _currentSteps) {
+    currentSteps = _currentSteps;
     if (state == State.Active && (currentSteps >= initialSteps + goalSteps) && now < completionDate) {
       state = State.Achieved;
     } else if (state == State.Active && (currentSteps < initialSteps + goalSteps) && now >= completionDate) {
       state = State.Failed;
     }
+
   }
 }
