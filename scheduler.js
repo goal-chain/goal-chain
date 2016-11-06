7dc89ef6a5d7a8cd13dae9e0510ebf35a95d0fdb
Conor Fennell <conor@Conors-MacBook-Pro.local>
2016-11-06 11:15:24 +0000

    update scheduler

--- scheduler.js
+++ scheduler.js
@@ -4,12 +4,20 @@ const Goal = require('./build/contracts/Goal.sol.js');
 const Web3 = require('web3');
 const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const web3 = new Web3(provider);
+const axios = require('axios');
 GoalFactory.setProvider(provider);
 Goal.setProvider(provider);
 const goalFactory = GoalFactory.deployed(); 
 
-new CronJob('* * * * * *', function() {
+new CronJob('*/24 * * * * *', function() {
   console.log('You will see this message every second');
+  axios.get('https://calm-reaches-82247.herokuapp.com/getsteps/eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyUU02TTkiLCJhdWQiOiIyMjdYSjMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyYWN0IiwiZXhwIjoxNDc4OTUwNjg0LCJpYXQiOjE0NzgzODg2MDZ9.RUb-YoIE0m9hhTRDuT_0JVtAqzdI5Jbiy8ntw72g8M0')
+    .then(result => console.log(result.data.steps));
+
+}, null, true, 'America/Los_Angeles');
+
+
+const retrieveAllExistingGoals = () => {
   const Unicef = '0x00219e9bff127d11da668ca92c0598c77e9f3e51';
   const AmnestyInternational =  '0xbde95d88cd32cdf46ed3d460edc940f523094445';
   const Sean =  '0x3c7904ff18c35f4faf721c858098e77a5329ccba';
@@ -24,6 +32,4 @@ new CronJob('* * * * * *', function() {
     })
     .then(console.log)
     .catch(console.log);
-
-
-}, null, true, 'America/Los_Angeles');
+}
